fec441fb8e1f53ab9f2f2c6379e06d09be320c3b
Daniel Anini Baah <charismaworld9@gmail.com>
2020-06-17 17:24:52 -0400

    understanding JS precedence

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ var y =12
 var z = x * y
 console.log(z);
 
+//precendence in js script
+console.log(6+4*5); // especting 26 meaning multiplication is solved before addition
+
+console.log(2+4**2); //expecting 18
+// meaning power is solved before addition
+
+let a;
+let b;
+console.log(a=b=5);
 
 
 
