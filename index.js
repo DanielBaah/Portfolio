cbc83d7aa7af77b0e004a9912f7345e1caa6eb68
Daniel Anini Baah <charismaworld9@gmail.com>
2020-06-21 02:12:18 -0400

    understanding "searching"

--- index.js
+++ index.js
@@ -16,10 +16,66 @@ let a;
 let b;
 console.log(a=b=5);
 
+//understanding sequence 
 
 
+    var t = 1;
+    var w = 2;
+    var u = 3;
+    t = w;
+    u = t;
+
+console.log(t);
+
+// understanding functions
+
+function sum(a,b){
+    return a+b;
+}
+let result =sum(3,4)
+console.log(result);
 
 
+// Understanding methods 
+var person= {
+    firstname:"Daniel",
+    Lastname: "Anini Baah",
+    id: 5566,
+    }
+    console.log(person); //
 
+// Understanding parameters;
+x = findMax (1,200,300,500,700);
+function findMax(){
+   var i;
+   var max = -Infinity;
+   for (i =0; i< arguments.length; i++) {
+       if (arguments [i] >max)
+       Max =arguments [i]; 
+    }
+}
+console.log(Max);
 
+//understanding pass by value
+function square(x) {
+    x= x*x; 
+}
+var y= 10;
+square(y);
+console.log(x); // the Y value is passed to x byt y remains unchanged.
+// if javascript was to run in pass by ref y wud have been 100.
 
+function turnOn(X) { //under standing passby ref
+    X.isOn = true;
+}
+var y = {
+isOn: false
+};
+turnOn(y);
+console.log(y);
+
+// Understanding searching 
+const alligator = ["thick scales", 80, "4 foot tail", "rounded snout"];
+
+alligator.includes("thick scales"); // returns true
+console.log(alligator.includes('thick scales'));
